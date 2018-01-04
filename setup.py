900d07eff0e4d7e1c46aded96aff0a6e30a8ac26
Marco Borchi <borchimarco@gmail.com>
2018-01-04 10:53:34 +0100

    Add on git

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='miao',
+      version='0.1',
+      description='Print a cat',
+      url='',
+      author='Trafitto',
+      author_email='develop@trafitto.com',
+      license='MIT',
+      packages=['miao'],
+      zip_safe=False)

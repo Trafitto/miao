94a38cec7e50e2c581ee142013692f8ed38dbc5a
Marco Borchi <borchimarco@gmail.com>
2018-01-04 15:08:00 +0100

    Publish on Pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+		
 setup(name='miao',
       version='0.1',
       description='Print a cat',
+	  long_description=readme(),
       url='https://github.com/Trafitto/miao',
       author='trafitto',
       author_email='develop@trafitto.com',
